c5c8424ad69cc377524bcc8990a173547d9a22d2
AlymovDaniil <christian.bale.new@gmail.com>
2024-02-07 19:10:35 +0300

    adding new files to file-managment

---
+++ Program/FileManagment/VideoAudioManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPPI_LR2_MODULES
+{
+    class VideoAudioManager
+    {
+        public VideoAudioManager() { }
+
+        /// <summary>
+        /// Loads videos asynchronously
+        /// </summary>
+        /// <returns>asynchronous task object</returns>
+        public async Task LoadVideo() { await Task.Delay(0); }
+
+        /// <summary>
+        /// Loads audio asynchronously
+        /// </summary>
+        /// <returns>asynchronous task object</returns>
+        public async Task LoadAudio() { await Task.Delay(0); }
+
+        /// <summary>
+        /// Saves video asynchronously
+        /// </summary>
+        /// <returns>asynchronous task object</returns>
+        public async Task SaveVideo() { await Task.Delay(0); }
+
+        /// <summary>
+        /// Saves audio asynchronously
+        /// </summary>
+        /// <returns>asynchronous task object</returns>
+        public async Task SaveAudio() { await Task.Delay(0); }
+    }
+}
