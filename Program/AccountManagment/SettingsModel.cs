b8e59637cfbcea6331d5255bbb58fe2d744d5a4b
AlymovDaniil <christian.bale.new@gmail.com>
2024-02-07 17:59:37 +0300

    adding account managment folder

---
+++ Program/AccountManagment/SettingsModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPPI_LR2_MODULES
+{
+    public class SettingsModel
+    {
+        public SettingsModel() { }
+        public void SetConfidential(ConfidentialInformation confidential) { }
+    }
+}
