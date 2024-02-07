7296213bdc50e97cf6bf69700e262482d16e3c95
AlymovDaniil <christian.bale.new@gmail.com>
2024-02-07 19:04:00 +0300

    adding new files to crypto-system

---
+++ Program/CryptoSystem/Cryptographer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPPI_LR2_MODULES
+{
+    public class Cryptographer
+    {
+        public Cryptographer()
+        {
+            
+        }
+
+        /// <summary>
+        /// Encrypts the specified data
+        /// </summary>
+        /// <param name="value">data that needs to be encrypted</param>
+        /// <returns>encrypted data in string</returns>
+        public string Encrypt(string value) { throw new NotImplementedException(); }
+
+        /// <summary>
+        /// Decrypts the specified data
+        /// </summary>
+        /// <param name="value">data that needs to be decrypted</param>
+        /// <returns>decrypted data in string</returns>
+        public string Decrypt(string value) { throw new NotImplementedException(); }
+
+        /// <summary>
+        /// Encrypts the specified data
+        /// </summary>
+        /// <param name="value">data that needs to be encrypted</param>
+        /// <returns>encrypted data in bytes</returns>
+        public byte[] Encrypt(byte[] value) { throw new NotImplementedException(); }
+
+        /// <summary>
+        /// Decrypts the specified data
+        /// </summary>
+        /// <param name="value">data that needs to be decrypted</param>
+        /// <returns>decrypted data in bytes</returns>
+        public byte[] Decrypt(byte[] value) { throw new NotImplementedException(); }
+    }
+}
