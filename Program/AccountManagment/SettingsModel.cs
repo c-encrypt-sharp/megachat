b1eabe78abf7b03e3bfd80becc278024836bfc55
AlymovDaniil <christian.bale.new@gmail.com>
2024-02-07 18:41:48 +0300

    adding comments SettingsModel.cs

--- Program/AccountManagment/SettingsModel.cs
+++ Program/AccountManagment/SettingsModel.cs
@@ -9,10 +9,31 @@ namespace PPPI_LR2_MODULES
     public class SettingsModel
     {
         public SettingsModel() { }
-        public bool EnabledNotify { get; set; } = true;      
+        public bool EnabledNotify { get; set; } = true;
+
+        /// <summary>
+        /// Sets the specified privacy data
+        /// </summary>
+        /// <param name="confidential">privacy dataset</param>
         public void SetConfidential(ConfidentialInformation confidential) { }
+
+        /// <summary>
+        /// The method makes the selected chat hidden
+        /// </summary>
+        /// <param name="name">chat name</param>
         public void HideChat(string name) { }
+
+        /// <summary>
+        /// Changes the topic of the selected chat
+        /// </summary>
+        /// <param name="name">chat name</param>
+        /// <param name="theme">theme name</param>
         public void ChangeVisualChat(string name, string theme) { }
+
+        /// <summary>
+        /// Hides the account page
+        /// </summary>
+        /// <param name="visible">visibility, if true (default), then hides, otherwise makes visible</param>
         public void SetPrivateAccount(bool visible = true) { }       
     }
 }
