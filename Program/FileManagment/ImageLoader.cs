1bb6da041ba54de87cfbe503fae891893440b2a9
AlymovDaniil <christian.bale.new@gmail.com>
2024-02-07 19:06:14 +0300

    adding new files to file-managment

---
+++ Program/FileManagment/ImageLoader.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPPI_LR2_MODULES
+{
+    public class ImageLoader
+    {
+        public ImageLoader() { }
+
+        /// <summary>
+        /// Loads an image for processing
+        /// </summary>
+        /// <param name="path">path to the downloaded file</param>
+        public void LoadImage(string path) { }
+
+        /// <summary>
+        /// Saves the image
+        /// </summary>
+        /// <param name="path">save path</param>
+        public void SaveImage(string path) { }
+    }
+}
