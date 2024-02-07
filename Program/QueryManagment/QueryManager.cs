940ca2f70c3367c5679168236ebdf88b5549a368
AlymovDaniil <christian.bale.new@gmail.com>
2024-02-07 19:08:12 +0300

    adding new files to query-managment

---
+++ Program/QueryManagment/QueryManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPPI_LR2_MODULES
+{
+    public class QueryManager
+    {
+        public QueryManager() { }
+        public QueryManager(string query) { }
+
+        /// <summary>
+        /// Checks the request for errors
+        /// </summary>
+        /// <exception cref="QuerySyntaxException"></exception>
+        public void CheckQuery() 
+        {
+            try
+            {
+
+            }
+            catch (QuerySyntaxException) 
+            { 
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Corrects the request
+        /// </summary>
+        /// <returns></returns>
+        public bool FixRequest()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
