15607ee9c18e78c6b6f30fa4b75e9fc82deaa53a
AlymovDaniil <christian.bale.new@gmail.com>
2024-02-07 18:56:57 +0300

    adding new files to communication-mail

---
+++ Program/MailCommunication/NotificationWorker.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPPI_LR2_MODULES
+{
+    public class NotificationWorker
+    {
+        /// <summary>
+        /// Waiting for notification
+        /// </summary>
+        /// <returns>asynchronous task object</returns>
+        public async Task ReceiveNotifyAsync() { await Task.Delay(0); }
+
+        /// <summary>
+        /// Creates a newsletter
+        /// </summary>
+        /// <param name="notification"></param>
+        /// <returns>asynchronous task object</returns>
+        public async Task SendNotifyAsync(NotificationModel notification) { await Task.Delay(0); }
+    }
+}
