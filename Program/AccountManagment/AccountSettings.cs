5211ff307679c0f40952f1538a80e450d939066a
AlymovDaniil <christian.bale.new@gmail.com>
2024-02-07 18:14:00 +0300

    adding account managment files

---
+++ Program/AccountManagment/AccountSettings.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPPI_LR2_MODULES
+{
+    public class AccountSettings
+    {
+        SettingsModel? _settings;
+
+        public SettingsModel? Settings { get => _settings; set => _settings = value; }
+
+        public AccountSettings() { }
+        /// <summary>
+        /// Method sets new settings
+        /// </summary>
+        /// <param name="settings">parameter represents a settings data model object</param>
+        public void SetSettings(SettingsModel settings) 
+        {
+
+        }
+        /// <summary>
+        /// Method reset setting
+        /// </summary>
+        public void ResetSettings()
+        {
+
+        }
+    }
+}
