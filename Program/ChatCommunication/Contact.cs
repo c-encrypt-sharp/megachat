8621e15624f1860f27f96387f94573d154a582fa
AlymovDaniil <christian.bale.new@gmail.com>
2024-02-07 18:59:56 +0300

    adding new files to communication-messages

---
+++ Program/ChatCommunication/Contact.cs
@@ -0,0 +1,7 @@
+﻿namespace PPPI_LR2_MODULES
+{
+    public class Contact
+    {
+        public override int GetHashCode() { throw new NotImplementedException(); }
+    }
+}
---
+++ Program/ChatCommunication/ContactManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPPI_LR2_MODULES
+{
+    public class ContactManager
+    {
+        public ContactManager() { }
+
+        /// <summary>
+        /// Adds the specified account
+        /// </summary>
+        /// <param name="contact">selected account</param>
+        public void AddContact(Contact contact) { }
+
+        /// <summary>
+        /// Delete the specified account
+        /// </summary>
+        /// <param name="contact">selected account</param>
+        public void RemoveContact(Contact contact) { }
+    }
+}
