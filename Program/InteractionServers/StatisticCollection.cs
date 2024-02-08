20cedf8f6351ded960022e56128737d8b309ab4a
AlymovDaniil <christian.bale.new@gmail.com>
2024-02-08 18:34:30 +0300

    modify StatisticCollection.cs

--- Program/InteractionServers/StatisticCollection.cs
+++ Program/InteractionServers/StatisticCollection.cs
@@ -7,10 +7,40 @@ using System.Threading.Tasks;
 
 namespace PPPI_LR2_MODULES
 {
-    public class StatisticCollection<T> : IEnumerable<T> where T : StatisticElement
+    public class StatisticCollection<T> : IEnumerable<T>, ICollection<T> where T : StatisticElement
     {
         public StatisticCollection() { }
+
+        public int Count => throw new NotImplementedException();
+
+        public bool IsReadOnly => throw new NotImplementedException();
+
+        public void Add(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
         public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(T item)
         {
             throw new NotImplementedException();
         }
