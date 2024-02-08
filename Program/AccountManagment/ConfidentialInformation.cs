49e549e45928478a68772ba29ace07b2ecd527d6
AlymovDaniil <christian.bale.new@gmail.com>
2024-02-08 18:44:03 +0300

    modify ConfidentialInformation.cs

--- Program/AccountManagment/ConfidentialInformation.cs
+++ Program/AccountManagment/ConfidentialInformation.cs
@@ -1,6 +1,40 @@
-﻿namespace PPPI_LR2_MODULES
+﻿using System.Runtime.Serialization;
+using System.Text.Json.Serialization;
+
+namespace PPPI_LR2_MODULES
 {
+    [DataContract]
     public class ConfidentialInformation
     {
+        [JsonConstructor]
+        public ConfidentialInformation(
+            bool visibilityPhone = false, 
+            bool visibilityOnline = false, 
+            WhoSendMe communicationWithMe = default, 
+            bool visibilityContacts = false){
+
+            VisibilityPhone = visibilityPhone;
+            VisibilityOnline = visibilityOnline;
+            CommunicationWithMe = communicationWithMe;
+            VisibilityContacts = visibilityContacts;
+        }
+        public enum WhoSendMe 
+        {
+            OnlyContacts,
+            All,
+            None
+        }
+
+        [DataMember]
+        public bool VisibilityPhone { get; set; }
+
+        [DataMember]
+        public bool VisibilityOnline { get; set; }
+
+        [DataMember]
+        public WhoSendMe CommunicationWithMe { get; set; }
+
+        [DataMember]
+        public bool VisibilityContacts { get; set; }
     }
 }
