851929b9b16458123648346aa5d40d0bb0f4316e
AlymovDaniil <christian.bale.new@gmail.com>
2024-02-08 18:59:05 +0300

    modify GroupMessagesWork.cs

--- Program/ChatCommunication/GroupMessagesWork.cs
+++ Program/ChatCommunication/GroupMessagesWork.cs
@@ -33,5 +33,26 @@ namespace PPPI_LR2_MODULES
         /// </summary>
         /// <param name="user">the user whose user needs to be deleted</param>
         public void DeleteUser(UserAccountModel user) { }
+
+        /// <summary>
+        /// Sends a message asynchronously
+        /// </summary>
+        /// <param name="messageModel">message for send</param>
+        /// <returns>asynchronous task object</returns>
+        public async Task SendMessagesAsync(MessageModel messageModel) { await new MessageManager().SendMessageAsync(messageModel); }
+
+        /// <summary>
+        /// Deletes a message asynchronously
+        /// </summary>
+        /// <param name="index">index of message</param>
+        /// <returns>asynchronous task object</returns>
+        public async Task DeleteMessagesAsync(int index) { await new MessageManager().DeleteMessageAsync(index); }
+
+        /// <summary>
+        /// Load messages asynchronously
+        /// </summary>
+        /// <param name="count">count messages</param>
+        /// <returns>asynchronous task object</returns>
+        public async Task LoadMessagesAsync(int count) { await new MessageManager().LoadMessagesAsync(count); }
     }
 }
