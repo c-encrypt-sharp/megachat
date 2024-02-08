30d728d890a25d5c61bc7155f0e40d1b9005a5ed
AlymovDaniil <christian.bale.new@gmail.com>
2024-02-08 18:49:32 +0300

    modify Contact.cs

--- Program/ChatCommunication/Contact.cs
+++ Program/ChatCommunication/Contact.cs
@@ -1,7 +1,27 @@
-﻿namespace PPPI_LR2_MODULES
+﻿using System.Runtime.Serialization;
+using System.Text.Json.Serialization;
+
+namespace PPPI_LR2_MODULES
 {
+    [DataContract]
     public class Contact
     {
+        [JsonConstructor]
+        public Contact(string name = null, string surname = null, string pageID = null)
+        {
+            Name = name;
+            Surname = surname;
+            PageID = pageID;
+        }
+
+        [DataMember]
+        public string Name { get; set; }
+
+        [DataMember]
+        public string Surname { get; set; }
+
+        [DataMember]
+        public string PageID { get; set; }
         public override int GetHashCode() { throw new NotImplementedException(); }
     }
 }
