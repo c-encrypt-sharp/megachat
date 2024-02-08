712cb968f349f01d310a8bac6f11397f89c8cc28
AlymovDaniil <christian.bale.new@gmail.com>
2024-02-08 19:11:31 +0300

    modify NotificationModel.cs

--- Program/MailCommunication/NotificationModel.cs
+++ Program/MailCommunication/NotificationModel.cs
@@ -1,6 +1,24 @@
-﻿namespace PPPI_LR2_MODULES
+﻿using System.Runtime.Serialization;
+using System.Text.Json.Serialization;
+
+namespace PPPI_LR2_MODULES
 {
+    [DataContract]
     public class NotificationModel
     {
+        [JsonConstructor]
+        public NotificationModel(string message = null, string sender = null, DateTime date = default)
+        {
+            Message = message;
+            Sender = sender;
+            Date = date;
+        }
+
+        [DataMember]
+        public string Message { get; set; }
+        [DataMember]
+        public string Sender { get; set; }
+        [DataMember]
+        public DateTime Date { get; set; }
     }
 }
