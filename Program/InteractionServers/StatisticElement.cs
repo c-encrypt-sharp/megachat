e6edfda95fb6c610bcd8a29673f7c48a3f414573
AlymovDaniil <christian.bale.new@gmail.com>
2024-02-08 18:11:28 +0300

    modify StatisticElement.cs

--- Program/InteractionServers/StatisticElement.cs
+++ Program/InteractionServers/StatisticElement.cs
@@ -2,5 +2,10 @@
 {
     public abstract class StatisticElement
     {
+        protected StatisticElement() { }
+        public int CountOnline { get; set; } 
+        public int TotalCountMessages { get; set; }
+        public int AvgMessagesInDay { get; set; }
+        public int AvgTimeOnline { get; set; }
     }
 }
